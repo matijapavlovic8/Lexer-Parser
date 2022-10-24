3c429ae79fa8f9a405814cf4ef7886a52b0682ec
matijapavlovic8 <101288349+matijapavlovic8@users.noreply.github.com>
2022-10-24 20:50:32 +0200

    Final

--- src/test/java/hr/fer/oprpp1/hw02/prob1/TestSmartScript.java
+++ src/test/java/hr/fer/oprpp1/hw02/prob1/TestSmartScript.java
@@ -3,6 +3,8 @@ package hr.fer.oprpp1.hw02.prob1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import hr.fer.oprpp1.custom.scripting.lexer.SmartScriptLexer;
+import hr.fer.oprpp1.custom.scripting.lexer.SmartScriptTokenType;
+import hr.fer.oprpp1.custom.scripting.nodes.Node;
 import hr.fer.oprpp1.custom.scripting.parser.SmartScriptParser;
 import org.junit.jupiter.api.Test;
 
@@ -25,10 +27,30 @@ public class TestSmartScript {
     }
 
     @Test
-
     public void testNumberOfChildren(){
         SmartScriptParser parser = new SmartScriptParser("document {$= tag $}");
         assertEquals(2, parser.getDocumentNode().numberOfChildren());
+    }
+
+    @Test
+    public void testNullAsArgumentNode(){
+        Node node = new Node();
+        assertThrows(NullPointerException.class, () -> node.addChildNode(null));
+    }
+
+    @Test
+    public void testGetToken(){
+        SmartScriptLexer lexer = new SmartScriptLexer("input");
+        assertThrows(LexerException.class, lexer::getToken);
+    }
+
+    @Test
+    public void testNextToken(){
+        SmartScriptLexer lexer = new SmartScriptLexer("input");
+        assertEquals(SmartScriptTokenType.TEXT, lexer.nextToken().getType());
+        assertEquals(SmartScriptTokenType.TEXT, lexer.getToken().getType());
+        assertEquals("input", lexer.getToken().getValue());
+        assertEquals(SmartScriptTokenType.EOF, lexer.nextToken().getType());
     }
 
     
